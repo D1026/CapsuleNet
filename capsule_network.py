5e463f31a93292ab573682ce4b024426375f46d8
Ivan_Duan <492987559@qq.com>
2019-02-19 10:06:15 +0800

    0

--- capsule_network.py
+++ capsule_network.py
@@ -10,37 +10,6 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 from torch.autograd import Variable
-import numpy as np
-
-BATCH_SIZE = 100
-NUM_CLASSES = 10
-NUM_EPOCHS = 500
-NUM_ROUTING_ITERATIONS = 3
-
-
-def softmax(input, dim=1):
-    transposed_input = input.transpose(dim, len(input.size()) - 1)
-    softmaxed_output = F.softmax(transposed_input.contiguous().view(-1, transposed_input.size(-1)), dim=-1)
-    return softmaxed_output.view(*transposed_input.size()).transpose(dim, len(input.size()) - 1)
-
-
-def augmentation(x, max_shift=2):
-    _, _, height, width = x.size()
-
-    h_shift, w_shift = np.random.randint(-max_shift, max_shift + 1, size=2)
-    source_height_slice = slice(max(0, h_shift), h_shift + height)
-    source_width_slice = slice(max(0, w_shift), w_shift + width)
-    target_height_slice = slice(max(0, -h_shift), -h_shift + height)
-    target_width_slice = slice(max(0, -w_shift), -w_shift + width)
-
-    shifted_image = torch.zeros(*x.size())
-    shifted_image[:, :, source_height_slice, source_width_slice] = x[:, :, target_height_slice, target_width_slice]
-    return shifted_image.float()
-
-# --- for checking ---
-# self.capsule_layer = capsule_network.CapsuleLayer(num_labels, 1, config.hidden_size, output_atoms,
-#                                                   num_iterations=num_iterations)
-# --- ---
 
 
 class Original_CapsuleLayer(nn.Module):
@@ -153,42 +122,6 @@ class CapsuleLayer(nn.Module):
 
         # return v
         return torch.transpose(v, 0, 2)  # just adapt original API
-
-class CapsuleNet(nn.Module):
-    def __init__(self):
-        super(CapsuleNet, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=256, kernel_size=9, stride=1)
-        self.primary_capsules = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=32,
-                                             kernel_size=9, stride=2)
-        self.digit_capsules = CapsuleLayer(num_capsules=NUM_CLASSES, num_route_nodes=32 * 6 * 6, in_channels=8,
-                                           out_channels=16)
-
-        self.decoder = nn.Sequential(
-            nn.Linear(16 * NUM_CLASSES, 512),
-            nn.ReLU(inplace=True),
-            nn.Linear(512, 1024),
-            nn.ReLU(inplace=True),
-            nn.Linear(1024, 784),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x, y=None):
-        x = F.relu(self.conv1(x), inplace=True)
-        x = self.primary_capsules(x)
-        x = self.digit_capsules(x).squeeze().transpose(0, 1)
-
-        classes = (x ** 2).sum(dim=-1) ** 0.5
-        classes = F.softmax(classes, dim=-1)
-
-        if y is None:
-            # In all batches, get the most active capsule.
-            _, max_length_indices = classes.max(dim=1)
-            y = Variable(torch.eye(NUM_CLASSES)).cuda().index_select(dim=0, index=max_length_indices.data)
-
-        reconstructions = self.decoder((x * y[:, :, None]).view(x.size(0), -1))
-
-        return classes, reconstructions
 
 
 class CapsuleLoss(nn.Module):
